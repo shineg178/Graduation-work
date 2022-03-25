d10b6e7a5194421b6d65d753af37a9c9ee775365
jwpark1104 <woojp1104@gmail.com>
2022-03-25 22:18:31 +0900

    profile

--- startbootstrap-sb-admin-gh-pages/js/UserProfile.js
+++ startbootstrap-sb-admin-gh-pages/js/UserProfile.js
@@ -1,5 +1,5 @@
-let today = new Date();   
 
+let today = new Date();   
 let year = today.getFullYear(); // 년도
 let month = today.getMonth() + 1;  // 월
 let date = today.getDate();  // 날짜
@@ -17,3 +17,19 @@ StoryDate3.innerText = `${year}.${month}.${date}`;
 function HomeBtn(){
 location.href ='index.html';
 }
+function LogOutBtn(){
+    location.href ='login.html';
+    }
+    function showPopUp() { 
+        //창 크기 지정 
+        var width = 500; var height = 500; 
+        //pc화면기준 가운데 정렬
+         var left = (window.screen.width / 2) - (width/2); var top = (window.screen.height / 4); 
+        //윈도우 속성 지정 
+        var windowStatus = 'width='+width+', height='+height+', left='+left+', top='+top+', scrollbars=yes, status=yes, resizable=yes, titlebar=yes'; 
+        //연결하고싶은url 
+        const url = "EditProfile.html";
+        //등록된 url 및 window 속성 기준으로 팝업창을 연다. 
+        window.open(url, "hello popup", windowStatus); }
+
+    
