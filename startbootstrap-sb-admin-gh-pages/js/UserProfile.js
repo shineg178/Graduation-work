5b4bb37e0b7f224c496943391b86893b13bf63be
jwpark1104 <woojp1104@gmail.com>
2022-03-25 16:22:47 +0900

    UserProfile

--- startbootstrap-sb-admin-gh-pages/js/UserProfile.js
+++ startbootstrap-sb-admin-gh-pages/js/UserProfile.js
@@ -8,10 +8,12 @@ let day = today.getDay();  // 요일
 document.write(year + '/' + month + '/' + date)
 document.write('<br>')
 document.write(day);
-const StoryDate =  document.getElementById("date");
+const StoryDate =  document.querySelector("#date");
 const StoryDate2 = document.getElementById("date2");
+const StoryDate3 = document.getElementById("date3");
 StoryDate.innerText = `${year}.${month}.${date}`;
 StoryDate2.innerText = `${year}.${month}.${date}`;
+StoryDate3.innerText = `${year}.${month}.${date}`;
 function HomeBtn(){
 location.href ='index.html';
 }
