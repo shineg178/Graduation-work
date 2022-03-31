2cf8c93d662d843253e10cd1d404ed96243cacc5
jwpark1104 <woojp1104@gmail.com>
2022-03-31 15:24:41 +0900

    User'sProfile

--- startbootstrap-sb-admin-gh-pages/js/UserProfile.js
+++ startbootstrap-sb-admin-gh-pages/js/UserProfile.js
@@ -4,7 +4,8 @@ let year = today.getFullYear(); // 년도
 let month = today.getMonth() + 1;  // 월
 let date = today.getDate();  // 날짜
 let day = today.getDay();  // 요일
-
+const UserId = document.getElementById("UserId");
+let UID = "박진우";
 document.write(year + '/' + month + '/' + date)
 document.write('<br>')
 document.write(day);
@@ -14,6 +15,7 @@ const StoryDate3 = document.getElementById("date3");
 StoryDate.innerText = `${year}.${month}.${date}`;
 StoryDate2.innerText = `${year}.${month}.${date}`;
 StoryDate3.innerText = `${year}.${month}.${date}`;
+UserId.innerText = `${UID} (${month}.${date})`;
 function HomeBtn(){
 location.href ='index.html';
 }
